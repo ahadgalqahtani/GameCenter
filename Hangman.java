677b5db80cc4399b5b1a8f8c20b53848fc069373
ahadgalqahtani <123299674+ahadgalqahtani@users.noreply.github.com>
2023-01-26 11:42:24 +0300

    Update Hangman.java

--- Hangman.java
+++ Hangman.java
@@ -2,16 +2,21 @@ import javax.swing.JOptionPane;
 
 public class Hangman{
 
-    private static String[] words = { "terminator", "banana", "computer", "cow", "rain", "water" }; // array of words to guess
+    private static String[] words = { "rice", "banana", "computer", "cow", "rain", "water" }; // array of words to guess
     private static String word = words[(int) (Math.random() * words.length)];
     private static String asterisk = new String(new char[word.length()]).replace("\0", "*");
     private static int count = 0;
+    int points = 0;
 
     public Hangman(){
-        JOptionPane.showMessageDialog(null, "WELCOME TO HANGMAN!");
-        JOptionPane.showMessageDialog(null, "To play this game you have to guess a word that" 
+        JOptionPane.showMessageDialog(null, 
+        "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\n" +
+        "    Welcome to the Hangman Game! .|^__^|. \n" +
+        "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\n",
+        "Hangman", 1);
+        JOptionPane.showMessageDialog(null, "To play this game you have to guess a word that " 
         + "the computer has generated for you by guessing the letters in it.");
-        JOptionPane.showMessageDialog(null, "For every wrong guess, a picture will be drawn." 
+        JOptionPane.showMessageDialog(null, "For every wrong guess, a picture will be drawn. " 
         + "If the picture is completed then you have lost.");
 
         while (count < 7 && asterisk.contains("*")) {
@@ -22,7 +27,7 @@ public class Hangman{
         }
     }
 
-    public void hang(String guess){
+    public int hang(String guess){
         String newasterisk = "";
         for (int i = 0; i < word.length(); i++) {
             if (word.charAt(i) == guess.charAt(0)) {
@@ -42,69 +47,67 @@ public class Hangman{
         }
         if (asterisk.equals(word)){
             JOptionPane.showMessageDialog(null, "Correct! You win! The word was " + word);
+            points += 6;
         }
+        return points;
     }
 
     public void hangmanImage() {
         if (count == 1) {
-            JOptionPane.showMessageDialog(null,"Wrong guess, try again" +
+            JOptionPane.showMessageDialog(null,"Wrong guess, try again\n" +
             "\n" + "\n" + "\n" + "\n" + "___|___" + "\n");
         }
         if (count == 2) {
-            JOptionPane.showMessageDialog(null,"Wrong guess, try again" +
-            "|\n" + "|\n" + "|\n" + "|\n" + "|\n" + "___|___");
+            JOptionPane.showMessageDialog(null,"Wrong guess, try again\n" +
+            "     |\n" + "     |\n" + "     |\n" + "     |\n" + "     |\n" + "___|___");
         }
         if (count == 3) {
-            JOptionPane.showMessageDialog(null,"Wrong guess, try again" +
-            "   ____________" + "   |\n" + "   |\n" + "   |\n" + "   |\n" + "   |\n" + "   |\n" +"   |\n" + "___|___");
+            JOptionPane.showMessageDialog(null,"Wrong guess, try again\n" +
+            "     __________\n" + 
+            "     |\n" + 
+            "     |\n" + 
+            "     |\n" + 
+            "     |\n" + 
+            "     |\n" + 
+            "     |\n" +
+            "     |\n" + 
+            "___|___");
         }
         if (count == 4) {
-            JOptionPane.showMessageDialog(null,"Wrong guess, try again" +
-            "   ____________" + 
-            "   |\n" + 
-            "   |\n" + 
-            "   |\n" + 
-            "   |\n" + 
-            "   |\n" + 
-            "   |\n" +
-            "   |\n" + 
+            JOptionPane.showMessageDialog(null,"Wrong guess, try again\n" +
+            "     ___________\n" +
+            "     |            _|_\n" + 
+            "     |           /   \\ \n" +
+            "     |          |      |\n" +
+            "     |          \\_ _/\n" +
+            "     |             |\n" +
+            "     |             |\n" +
+            "     |\n" +
             "___|___");
         }
         if (count == 5) {
             JOptionPane.showMessageDialog(null,"Wrong guess, try again" +
-            "   ____________" +
-            "   |          _|_\n" + 
-            "   |         /   \\ \n" +
-            "   |        |     |\n" +
-            "   |         \\_ _/\n" +
-            "   |           |\n" +
-            "   |           |\n" +
-            "   |\n" +
-            "___|___");
+            "\n   ____________\n" +
+            "     |            _|_\n" + 
+            "     |           /   \\ \n" +
+            "     |          |      |\n" +
+            "     |          \\_ _/\n" +
+            "     |             |\n" +
+            "     |             |\n" +
+            "     |            / \\ \n" +
+            "___|___     /   \\");
         }
         if (count == 6) {
-            JOptionPane.showMessageDialog(null,"Wrong guess, try again" +
-            "   ____________" +
-            "   |          _|_\n" + 
-            "   |         /   \\ \n" +
-            "   |        |     |\n" +
-            "   |         \\_ _/\n" +
-            "   |           |\n" +
-            "   |           |\n" +
-            "   |          / \\ \n" +
-            "___|___      /   \\");
-        }
-        if (count == 7) {
-            JOptionPane.showMessageDialog(null,"Wrong guess, try again" +
-            "   ____________" +
-            "   |          _|_\n" + 
-            "   |         /   \\ \n" +
-            "   |        |     |\n" +
-            "   |         \\_ _/\n" +
-            "   |          _|_ \n" +
-            "   |         / | \\ \n" +
-            "   |          / \\ \n" +
-            "___|___      /   \\ \n" + 
+            JOptionPane.showMessageDialog(null,"Wrong guess, try again\n" +
+            "     ___________\n" +
+            "     |           _|_\n" + 
+            "     |          /   \\ \n" +
+            "     |         |      |\n" +
+            "     |         \\_ _/\n" +
+            "     |           _|_ \n" +
+            "     |          / | \\ \n" +
+            "     |           / \\ \n" +
+            "___|___    /   \\ \n" + 
             "GAME OVER! The word was " + word);
         }
     }
