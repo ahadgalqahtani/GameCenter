84c2851074fc38b1cba33531626965ddc9ea959d
ahadgalqahtani <123299674+ahadgalqahtani@users.noreply.github.com>
2023-01-30 14:41:21 +0300

    Add files via upload

---
+++ Information.java
@@ -0,0 +1,53 @@
+/**
+ * The class Information stores data
+ * about the project details and the
+ * creators of the project.
+ */
+
+ public class Information{
+    private ProjectInfo projectInfo; // project information
+    private StudentInfo studentInfo; // student information
+
+    /**
+     * This constructor initializes the 
+     * student information and the projects information
+     * @param projectInfo a ProjectInfo object
+     * @param studentInfo a StudentInfo object
+     */
+
+    public Information(ProjectInfo proInfo, StudentInfo stutInfo){
+        projectInfo = new ProjectInfo(proInfo);
+        studentInfo = new StudentInfo(stutInfo);    }
+
+    /**
+     * A getProInfo method
+     * @return A reference to a copy of the
+     * project information
+     */
+
+    public ProjectInfo getProInfo(){
+        return new ProjectInfo(projectInfo);
+    }
+
+     /**
+     * A getStuInfo method
+     * @return A reference to a copy of this
+     * projects student's information
+     */
+
+    public StudentInfo getStuInfo(){
+        return new StudentInfo(studentInfo);
+    } 
+
+    /**
+     * A toString method
+     * @return  A string containing all of the information
+     * stored in the classes
+     */
+
+     public String toString(){
+        String str = "Project Information: " + projectInfo + "\n\nStudent Information: \n" + studentInfo;
+
+        return str;
+     }
+ }
